c182a2b603de56a86c12c7bad6f9a819a32313de
EmperMiner <6thanhdat@gmail.com>
2022-07-31 12:01:14 +0700

    Added Dice Torque and Normalized Dice Speed

--- KyubuWheato/Assets/Scripts/diceScript.cs
+++ KyubuWheato/Assets/Scripts/diceScript.cs
@@ -6,6 +6,7 @@ public class diceScript : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float fireForce = 5;
+    public float spinForce = 1;
 
     void Start()
     {
@@ -15,14 +16,18 @@ public class diceScript : MonoBehaviour
 
         Vector3 direction = mousePos - transform.position;
         Vector3 rotation = transform.position - mousePos;
-        rb.velocity = new Vector2(direction.x, direction.y) * fireForce;
+
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * fireForce;
+        AddTorqueImpulse(spinForce);
+
         float rot = (Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg) - 90f;
         transform.rotation = Quaternion.Euler(0, 0, rot);
     }
 
     
-    void Update()
+    public void AddTorqueImpulse(float angularChangeInDegrees)
     {
-        
+        var impulse = (angularChangeInDegrees * Mathf.Deg2Rad) * rb.inertia;
+        rb.AddTorque(impulse, ForceMode2D.Impulse);
     }
 }
